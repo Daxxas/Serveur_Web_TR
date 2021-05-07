4d1521a730a647380b0bce13f46031e036e27ffa
Bbastpro <proust.bastien@gmail.com>
2021-05-07 15:50:40 +0200

    preparation

--- JS_Interne/export.js
+++ JS_Interne/export.js
@@ -1,35 +1,7 @@
 //Export format CSV
 document.getElementById('btn_exportcsv').addEventListener("click",function()
 {
-    var data = []
-    Register.Sensors.forEach(Sensor => {
-        var cles = Array.from(Sensor.dataset.keys())
-        cles.forEach(cle => {
-            data.push(new Array)
-            data[data.length-1].push(Sensor.id)
-            data[data.length-1].push(cle)
-            var values = ""
-                Sensor.dataset.get(cle).forEach(value => {
-                    values += (value[0]+"-"+value[1]) + ";"
-                });
-            values = values.slice(0,-1)
-            data[data.length-1].push(values)
-    });
-});
-
-    var csv = 'ID,TYPE,VALUE\n';
-    data.sort(function(a, b){return a[0] - b[0]});
-    data.forEach(function(row) {
-            csv += row.join(',');
-            csv += "\n";
-    });
-
-    console.log(csv);
-    var hiddenElement = document.createElement('a');
-    hiddenElement.href = 'data:text/csv;charset=utf-8,' + encodeURI(csv);
-    hiddenElement.target = '_blank';
-    hiddenElement.download = 'data.csv';
-    hiddenElement.click();
+    
 });
 
 //Export format Image
