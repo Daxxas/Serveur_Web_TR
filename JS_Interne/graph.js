a2fb896cca4615a5f06fc28706a1f9e3c045d672
Jacques-Portable <cleryja@gmail.com>
2021-06-14 10:36:06 +0200

    Update graph.js

--- JS_Interne/graph.js
+++ JS_Interne/graph.js
@@ -14,17 +14,6 @@ var chart = new Chart(ctx, {
     
     // Configuration options go here
     options: {
-        scales: {
-        yAxes: [{
-                            display: true,
-                            ticks: {
-                                min: 0,
-                                steps: 10,
-                                stepValue: 5,
-                                max: 150
-                            }
-                        }]
-        },
          plugins: {
           zoom: {
             zoom: {
